326c3b888e8e05297b643aa3aa2a39d86120b94e
Sean Loyless <loyless.sean@gmail.com>
2023-09-01 15:53:27 -0400

    Updating homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next';
+import Image from 'next/image';
 
 import { Layout } from 'layout';
 
@@ -7,9 +8,42 @@ const Home: NextPage = () => {
     <Layout>
       <main className="p-4">
         <div className="container mx-auto flex flex-col">
-          <p className="mt-4 text-center text-lg">
-            I&apos;ll gladly pay you Tuesday for a Hamburger today...
-          </p>
+          <h1 className="mb-3 mt-4 text-3xl font-bold">
+            It&apos;s BurgerTime!
+          </h1>
+          <div className="md:flex">
+            <div className="relative mx-auto min-h-[200px] w-full md:w-1/4">
+              <Image
+                src="/squidward.gif"
+                alt="Many burgers"
+                fill
+                priority
+                objectFit="cover"
+              />
+            </div>
+            <div className="w-full md:w-3/4 md:px-3">
+              <h2 className="mb-3 text-xl font-bold">
+                Do you like hamburgers?
+              </h2>
+              <p>
+                BurgerTime is a catalog of all the burgers you eat. Enter the
+                location, details about the burger, and rate it on a 6 point
+                scale!
+              </p>
+
+              <h2 className="my-3 text-xl font-bold">Features:</h2>
+              <ul className="list-disc">
+                <li className="ms-5">
+                  Dashboard featuring burgers sorted by recent.{' '}
+                  <em>(Coming Soon: Sort by Score!)</em>
+                </li>
+                <li className="ms-5">
+                  Add and review your burgers with a 6-pt scale.
+                </li>
+                <li className="ms-5">View your past burgers.</li>
+              </ul>
+            </div>
+          </div>
         </div>
       </main>
     </Layout>
